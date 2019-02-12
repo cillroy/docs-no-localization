56da628e0471f1e25576b946c5479a5f4f5bae71
Cillroy <curt@cullens.com>
2019-02-11 16:59:41 -0800

    get language picker working

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,65 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { ETIMEDOUT } from 'constants';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('extension.docs-no-localization', () => {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "docs-no-localization" is now active!');
+		const editor: vscode.TextEditor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		if (editor !== undefined) {
+			const lang = editor.document.languageId;
+			const selection = editor.selection;
+			const text = editor.document.getText(selection);
+			let newText: string;
+			switch (lang) {
+				case "yaml":
+					newText = 'noloc:\r\n- ' + (text.length <= 0 ? 'Word-To-Not-Localize' : text);
+					break;
+				case "markdown":
+					newText = ':::noloc text="' + (text.length <= 0 ? 'Word-To-Not-Localize' : text) + '":::';
+					break;
+				default:
+					const items: vscode.QuickPickItem[] = [{
+						'label': 'markdown',
+						'description': 'add placeholder for markdown',
+						'detail': 'markdown'
+					},
+					{
+						'label': 'metadata',
+						'description': 'add placeholder for metada',
+						'detail': 'metadata'
+					},
+					{
+						'label': 'yaml',
+						'description': 'add placeholder for yaml',
+						'detail': 'yaml'
+					}];
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+					const options: vscode.QuickPickOptions = {
+						placeHolder: 'Select which type of document you are working with'
+					};
+					vscode.window.showQuickPick(items, options).then(language => {
+						switch (language.detail) {
+							case "markdown":
+								newText = ':::noloc text="' + (text.length <= 0 ? 'Word-To-Not-Localize' : text) + '":::';
+								break;
+							default:
+								newText = 'noloc:\r\n- ' + (text.length <= 0 ? 'Word-To-Not-Localize' : text);
+								break;
+						}
+						editor.edit(builder => builder.replace(selection, newText));
+					});
+
+					break;
+			}
+			if (newText.length > 0) {
+				editor.edit(builder => builder.replace(selection, newText));
+			}
+		} else {
+			vscode.window.showErrorMessage('You must have a file open to use the `Docs No Localization` extension');
+		}
 	});
-
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
